a5612b687725a3eef3cdf4c03ed7878c52855d2e
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-05-12 12:35:59 +0200

    Use real colors.

--- src/scripts/load_data.js
+++ src/scripts/load_data.js
@@ -7,13 +7,12 @@ export default ({
   topoLayerName,
   colors
 }) => {
-  const _colors = Object.values(colors)
   const data = topojson.feature(geoSource, geoSource.objects[topoLayerName])
   data.features.map(f => {
     f.data = wedData[f.properties.AGS]
     f.data.name = f.properties.GEN
     f.data.bez = f.properties.BEZ
-    f.color = _colors[Math.floor(Math.random() * _colors.length)]
+    f.color = colors[f.data.c]
     f.data.id = f.properties.AGS
   })
   return data
